2eaeb980f2ea2bc6d064115a142128639d98bf58
Nicolas Diz <diz.nicolasamor@gmail.com>
2022-03-27 20:04:12 -0300

    MASTER: update BACKEND

--- backend/scripts/deploy.js
+++ backend/scripts/deploy.js
@@ -1,37 +1,12 @@
-// We require the Hardhat Runtime Environment explicitly here. This is optional
-// but useful for running the script in a standalone fashion through `node <script>`.
-//
-// When running the script with `npx hardhat run <script>` you'll find the Hardhat
-// Runtime Environment's members available in the global scope.
 const hre = require("hardhat");
-const { name, symbol, initialSupply } = require("./deploySettings");
 
 async function main() {
-  // Hardhat always runs the compile task when running scripts with its command
-  // line interface.
-  //
-  // If this script is run directly using `node` you may want to call compile
-  // manually to make sure everything is compiled
-  // await hre.run('compile');
+  const NFTMarketplace = await hre.ethers.getContractFactory("NFTMarketplace");
+  const nftMarketplace = await NFTMarketplace.deploy();
 
-  // We get the contract to deploy
-  const MyToken = await hre.ethers.getContractFactory("MyToken");
-  const token = await MyToken.deploy(name, symbol, initialSupply);
+  await nftMarketplace.deployed();
 
-  console.log("Deploying Token...");
-  await token.deployed();
-
-  console.log("MyToken deployed to:", token.address);
-
-  const tokenName = await token.name();
-  const tokenSymbol = await token.symbol();
-  const tokenTotalSupply = await token.totalSupply();
-
-  console.log(`Token name: ${tokenName}`);
-  console.log(`Token symbol: ${tokenSymbol}`);
-  console.log(`Total supply: ${tokenTotalSupply}`);
-
-  return token;
+  console.log("nftMarketplace deployed to:", nftMarketplace.address);
 }
 
 // We recommend this pattern to be able to use async/await everywhere
--- backend/scripts/deploySettings.js
+++
@@ -1,7 +0,0 @@
-const { ethers } = require("hardhat");
-
-module.exports = {
-  name: "My Token",
-  symbol: "MY_TOKEN",
-  initialSupply: ethers.utils.parseUnits("100"),
-};
