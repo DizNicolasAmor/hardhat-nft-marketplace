624733a7a6eae38051a8ae045d0ebb9f84e00b9a
Nicolas Diz <diz.nicolasamor@gmail.com>
2022-04-13 18:55:25 -0300

    MASTER: update populateMarketplace.js in BACKEND

--- backend/scripts/populateMarketplace.js
+++ backend/scripts/populateMarketplace.js
@@ -2,14 +2,9 @@ const LOCALHOST_CHAIN_ID = 31337;
 const CONTRACT_ADDRESSES = {
   [LOCALHOST_CHAIN_ID]: "0x5FbDB2315678afecb367f032d93F642f64180aa3",
 };
-const dogImagesURI = {
-  PUG: "https://ipfs.io/ipfs/QmSsYRx3LpDAb1GZQm7zZ1AuHZjfbPkD6J7s9r41xu1mf8?filename=pug.png",
-  SHIBA_INU:
-    "https://ipfs.io/ipfs/QmYx6GsYAKnNzZ9A6NvEKV9nf1VaDzJrqDR23Y8YSkebLU?filename=shiba-inu.png",
-  ST_BERNARD:
-    "https://ipfs.io/ipfs/QmUPjADFGEKmfohdTaNcWhp7VGk26h5jXDA7v3VtTnTLcW?filename=st-bernard.png",
-};
-const imagesURI = Object.values(dogImagesURI);
+const imageURI =
+  "https://ipfs.io/ipfs/bafybeig37ioir76s7mg5oobetncojcm3c3hxasyd4rvid4jqhy4gkaheg4/?filename=0-PUG.json";
+const quantityOfNFTs = 3;
 
 async function populateMarketplace() {
   const contractAddress = CONTRACT_ADDRESSES[LOCALHOST_CHAIN_ID];
@@ -22,10 +17,12 @@ async function populateMarketplace() {
   const value = listingPrice.toString();
 
   const nfts = await Promise.all(
-    imagesURI.map(async (uri) => {
+    new Array(quantityOfNFTs).fill(imageURI).map(async (uri) => {
       const nft = await nftMarketplace.createToken(uri, auctionPrice, {
         value,
       });
+      await nft.wait(1);
+
       return nft;
     })
   );
