4c65fc11b282a50911c80afa334245ed3aeb6c06
Nicolas Diz <diz.nicolasamor@gmail.com>
2022-04-16 11:34:36 -0300

    MASTER: update Item.tsx in FRONTEND

--- frontend/src/components/Item.tsx
+++ frontend/src/components/Item.tsx
@@ -1,14 +1,16 @@
 import React, { FC } from 'react';
 import { Button, Card } from 'react-bootstrap';
+import { useSelector } from 'react-redux';
+import { getUser } from '../redux/slices/userSlice';
 
 export interface IItem {
   description: string;
   handleClick: () => void;
   image: string;
   name: string;
-  owner?: string;
+  owner: string;
   price: string;
-  seller?: string;
+  seller: string;
   tokenId: string;
 }
 
@@ -19,18 +21,41 @@ const Item: FC<IItem> = ({
   name,
   price,
   tokenId,
+  owner,
+  seller,
   ...rest
-}) => (
-  <Card style={{ width: '18rem', margin: '1rem' }} id={tokenId} {...rest}>
-    <Card.Img variant="top" src={image} />
-    <Card.Body>
-      <Card.Title>{name}</Card.Title>
-      <Card.Text>{description}</Card.Text>
-      <Card.Text>{price} ETH</Card.Text>
-      <Button variant="primary" onClick={handleClick}>
-        BUY
-      </Button>
-    </Card.Body>
-  </Card>
-);
+}) => {
+  const { address } = useSelector(getUser);
+  const isButtonDisabled = address === owner || address === seller;
+  let buttonText;
+  switch (address) {
+    case owner:
+      buttonText = 'You are the owner';
+      break;
+    case seller:
+      buttonText = 'You are the seller';
+      break;
+    default:
+      buttonText = 'BUY';
+  }
+
+  return (
+    <Card style={{ width: '18rem', margin: '1rem' }} id={tokenId} {...rest}>
+      <Card.Img variant="top" src={image} />
+      <Card.Body>
+        <Card.Title>{name}</Card.Title>
+        <Card.Text>{description}</Card.Text>
+        <Card.Text>{price} ETH</Card.Text>
+        <Button
+          variant="primary"
+          onClick={handleClick}
+          disabled={isButtonDisabled}
+        >
+          {buttonText}
+        </Button>
+      </Card.Body>
+    </Card>
+  );
+};
+
 export default Item;
