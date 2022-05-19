49b66509f9e66eb54549834dcfc3eb117e73ac81
Nicolas Diz <diz.nicolasamor@gmail.com>
2022-05-19 15:43:26 -0300

    MASTER: update Item.tsx in FRONTEND

--- frontend/src/components/Item.tsx
+++ frontend/src/components/Item.tsx
@@ -2,6 +2,8 @@ import React, { FC } from 'react';
 import { Button, Card } from 'react-bootstrap';
 import { useSelector } from 'react-redux';
 import { getUser } from '../redux/slices/userSlice';
+import { getNetwork } from '../redux/slices/networkSlice';
+import _truncate from 'lodash.truncate';
 
 export interface IItem {
   description: string;
@@ -26,6 +28,9 @@ const Item: FC<IItem> = ({
   ...rest
 }) => {
   const { address } = useSelector(getUser);
+  const { symbol } = useSelector(getNetwork);
+  const truncatedName = _truncate(name, { length: 15 });
+
   const isButtonDisabled = address === owner || address === seller;
   let buttonText;
   switch (address) {
@@ -40,17 +45,23 @@ const Item: FC<IItem> = ({
   }
 
   return (
-    <Card style={{ width: '18rem', margin: '1rem' }} id={tokenId} {...rest}>
+    <Card
+      style={{ width: '18rem', margin: '1rem', overflow: 'hidden' }}
+      id={tokenId}
+      {...rest}
+    >
       <div
         className="d-flex align-items-center"
         style={{ width: '285px', height: '285px', overflow: 'hidden' }}
       >
         <Card.Img variant="top" src={image} />
       </div>
-      <Card.Body>
-        <Card.Title>{name}</Card.Title>
+      <Card.Body className="d-flex flex-column justify-content-between">
+        <Card.Title>{truncatedName}</Card.Title>
         <Card.Text>{description}</Card.Text>
-        <Card.Text>{price} ETH</Card.Text>
+        <Card.Text>
+          {price} {symbol}
+        </Card.Text>
         <Button
           variant="primary"
           onClick={handleClick}
