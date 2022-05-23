e48d7f24165b1d3a3dbc85f0a333a1c471adfc24
Nicolas Diz <diz.nicolasamor@gmail.com>
2022-05-23 18:07:59 -0300

    MASTER: update useMarketplace in FRONTEND

--- frontend/src/hooks/useMarketplace.tsx
+++ frontend/src/hooks/useMarketplace.tsx
@@ -1,40 +1,21 @@
-import { useEffect, useState } from 'react';
 import { Contract, ethers, providers } from 'ethers';
 import axios from 'axios';
 import NFTMarketplace from '../../artifacts/contracts/NFTMarketplace.sol/NFTMarketplace.json';
-import { CONTRACT_ADDRESSES, LOCALHOST_CHAIN_ID } from '../utils/constants';
+import { CONTRACT_ADDRESSES } from '../utils/constants';
 
 const useMarketplace = (chainId: number) => {
-  const [contractAddress, setContractAddress] = useState<string>(
-    CONTRACT_ADDRESSES[LOCALHOST_CHAIN_ID]
-  );
-  const [contractInstance, setContractInstance] = useState<
-    Contract | undefined
-  >(undefined);
-
-  useEffect(() => {
-    const newAddress: string = CONTRACT_ADDRESSES[chainId];
-    if (newAddress) {
-      setContractAddress(newAddress);
-    } else {
-      console.warn(
-        `Contract address not found in network with chainId: ${chainId}`
-      );
-    }
-  }, [chainId]);
+  const contractInstancesMap: { [key: number]: Contract | null } = {};
 
   const getContract = async () => {
-    if (contractInstance) return contractInstance;
-
     try {
       const provider = new providers.Web3Provider(window.ethereum);
       const signer = provider.getSigner();
       const instantiatedContract = new ethers.Contract(
-        contractAddress,
+        CONTRACT_ADDRESSES[chainId],
         NFTMarketplace.abi,
         signer
       );
-      setContractInstance(instantiatedContract);
+      contractInstancesMap[chainId] = instantiatedContract;
 
       return instantiatedContract;
     } catch (error) {
